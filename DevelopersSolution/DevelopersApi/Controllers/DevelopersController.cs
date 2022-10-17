a42489d87675eaf4210712662dcf38f11c868149
Nick Morris <progmorris20@gmail.com>
2022-10-17 15:06:25 -0400

    Before Break

---
+++ DevelopersSolution/DevelopersApi/Controllers/DevelopersController.cs
@@ -0,0 +1,35 @@
+﻿using DevelopersApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevelopersApi.Controllers;
+public class DevelopersController : ControllerBase
+{
+    //GET On-call developer
+    [HttpGet("/on-call-developer")]
+    public ActionResult GetOnCallDeveloper()
+    {
+        var response = new DeveloperDetailsModel("1", "Jeff", "Gonzalez", "555-1212", "jeff@hypertheory.com");
+        return Ok(response); //200
+    }
+
+    [HttpGet("/developers")]
+    public ActionResult GetAllDevelopers()
+    {
+        var response = new CollectionResponse<DeveloperSummaryModel>();
+        response.Data = new List<DeveloperSummaryModel>()
+        {
+            new DeveloperSummaryModel("1", "Jeff", "Gonzalez", "jeff@hypertheory.com"),
+            new DeveloperSummaryModel("2", "Sue", "Jones", "sue@aol.com")
+        };
+        return Ok(response);
+    }
+
+    [HttpPost("/developers")]
+    public ActionResult AddADeveloper([FromBody] DeveloperCreateModel request)
+    {
+        var response = new DeveloperDetailsModel(Guid.NewGuid().ToString(), request.FirstName, request.LastName, request.Email, request.Phone);
+        return StatusCode(201, response); // "Good. Ok. I created this.
+    }
+}
+
+
---
+++ DevelopersSolution/DevelopersApi/Models/DeveloperModels.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DevelopersApi.Models;
+public record DeveloperDetailsModel(string Id, string FirstName, string LastName, string Phone, string EMail);
+public record DeveloperSummaryModel(string Id, string FirstName, string LastName, string Email);
+
+public class CollectionResponse<T>{
+    public List<T> Data { get; set; } = new();
+}
+
+public record DeveloperCreateModel(string FirstName, string LastName, string Email, string Phone);
+
